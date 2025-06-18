7a414e05ceb52eac7f8b35959333b22b115a9b1c
Inaki Villar <inaki.seri@gmail.com>
2025-06-17 19:26:00 -0700

    adding remote execution

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,3 +3,14 @@ plugins {
     id("com.autonomousapps.dependency-analysis") version "2.18.0" apply true
 
 }
+allprojects {
+    tasks.withType<Test>().configureEach {
+        useJUnitPlatform()
+        develocity.testDistribution {
+            enabled.set(true)
+            remoteExecutionPreferred.set(true)
+            maxLocalExecutors.set(0)
+        }
+
+    }
+}
